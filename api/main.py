8f8d8e41a4982519c0d477e35662046c5d8cb31b
AS1 <arseniykofanov@gmail.com>
2025-11-08 14:51:49 +0300

    Тесты контрактов и фикс. Контракт нельзя и не нужно исполнять в первых ход

--- api/main.py
+++ api/main.py
@@ -58,7 +58,7 @@ async def lifespan(app: FastAPI):
 
     asyncio.create_task(scheduler.start())
     if debug:
-        asyncio.create_task(test2())
+        asyncio.create_task(test3())
 
     yield
 
@@ -293,3 +293,65 @@ async def test2():
     
     # print("Credit taken:", res)
     # # 666 коммит моооооой by AS1
+    
+
+async def test3():
+    
+    from game.user import User
+    from game.session import Session, session_manager, SessionStages
+    from game.company import Company
+    from game.contract import Contract
+
+    # Очистка и создание сессии
+    if await session_manager.get_session('test3'):
+        session = await session_manager.get_session('test3')
+        if session: await session.delete()
+
+    session = await session_manager.create_session('test3')
+
+    await sleep(2)
+
+    print("Session created")
+
+    user1 = await User().create(
+        1,
+        session_id=session.session_id,
+        username="User 1"
+    )
+
+    user2 = await User().create(
+        2,
+        session_id=session.session_id,
+        username="User 2"
+    )
+
+    comp1 = await user1.create_company("Company 1")
+    comp2 = await user2.create_company("Company 2")
+    
+    await comp1.add_resource("wood", 20, True)
+    # await comp2.add_balance(20000)
+    
+    await session.update_stage(SessionStages.CellSelect)
+    
+    await session.update_stage(SessionStages.Game)
+    for i in [session, comp1, comp2]:
+        await i.reupdate()
+
+    cr = await Contract().create(
+        supplier_company_id=comp1.id,
+        customer_company_id=comp2.id,
+        session_id=session.session_id,
+        who_creator=comp1.id,
+        resource="wood",
+        amount_per_turn=10,
+        duration_turns=2,
+        payment_amount=10000
+    )
+    
+    await cr.accept_contract(comp2.id)
+    
+    await session.update_stage(SessionStages.ChangeTurn)
+    
+    await session.update_stage(SessionStages.Game)
+    for i in [session, comp1, comp2]:
+        await i.reupdate()
