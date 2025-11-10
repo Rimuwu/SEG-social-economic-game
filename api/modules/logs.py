194b280c099aea7f2572d54a26f2a08c5e445fa8
AS1 <arseniykofanov@gmail.com>
2025-11-10 23:49:12 +0300

    все логи

--- api/modules/logs.py
+++ api/modules/logs.py
@@ -2,8 +2,8 @@ from global_modules.logs import Logger
 
 
 game_logger = Logger().get_logger("game")
-
 routers_logger = Logger().get_logger("routers")
 websocket_logger = Logger().get_logger("websocket")
-websocket_logger.setLevel("WARNING")
-routers_logger.setLevel("WARNING")
+
+# websocket_logger.setLevel("WARNING")
+# routers_logger.setLevel("WARNING")
